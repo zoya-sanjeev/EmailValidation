8fb7dea2205620a183ce96f9c963e116a8aac98c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-07 23:12:39 +0530

    ADDED: first mandatory part of emailValidation

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -1,10 +1,24 @@
 package com.bridgelabz.emailvalidation;
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class EmailValidation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static boolean emailValidation(String emailAddress) {
+		String emailRegex = "^[a-zA-Z]{3}";
+		return Pattern.matches(emailRegex,emailAddress);
+	}
 
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		
+		System.out.println("Enter email");
+		String email=scanner.nextLine();
+		while(!emailValidation(email)) {
+			System.out.println("Invalid format. Enter again");
+			email=scanner.nextLine();
+		}
+		
 	}
 
 }
